1899b58ee83e3d1ebe9a7e58b60a117a10213219
wakky-alcedo <wakky.alcedo@gmail.com>
2024-12-04 14:39:29 +0900

    docs(matter-esp32c3): コメントを日本語にした

--- sandbox/esp32-arduino-matter-seeed_xiao_esp32c3/src/Light.cpp
+++ sandbox/esp32-arduino-matter-seeed_xiao_esp32c3/src/Light.cpp
@@ -8,50 +8,49 @@ using namespace esp_matter;
 using namespace esp_matter::endpoint;
 
 /**
- * This program presents example Matter light device with OnOff cluster by
- * controlling LED with Matter and toggle button.
+ * このプログラムは、LEDをMatterとトグルボタンで制御することにより、OnOffクラスターを持つMatterライトデバイスの例を示します。
  *
- * If your ESP does not have buildin LED, please connect it to LED_PIN
+ * ESPにビルドインLEDがない場合は、LED_PINに接続してください。
  *
- * You can toggle light by both:
- *  - Matter (via CHIPTool or other Matter controller)
- *  - toggle button (by default attached to GPIO0 - reset button, with debouncing) 
+ * ライトをトグルする方法は以下の通りです：
+ *  - Matter（CHIPToolや他のMatterコントローラーを介して）
+ *  - トグルボタン（デフォルトではGPIO0 - リセットボタンに接続され、デバウンス処理が行われます）
  */
 
-// Please configure your PINs
+// PINを設定してください
 const int LED_PIN = D0;
 const int TOGGLE_BUTTON_PIN = D9;
 
-// Debounce for toggle button
+// トグルボタンのデバウンス
 const int DEBOUNCE_DELAY = 500;
 int last_toggle;
 
-// Cluster and attribute ID used by Matter light device
+// Matterライトデバイスで使用されるクラスターと属性ID
 const uint32_t CLUSTER_ID = OnOff::Id;
 const uint32_t ATTRIBUTE_ID = OnOff::Attributes::OnOff::Id;
 
-// Endpoint and attribute ref that will be assigned to Matter device
+// Matterデバイスに割り当てられるエンドポイントと属性参照
 uint16_t light_endpoint_id = 0;
 attribute_t *attribute_ref;
 
-// There is possibility to listen for various device events, related for example to setup process
-// Leaved as empty for simplicity
+// セットアッププロセスに関連するさまざまなデバイスイベントをリッスンする可能性があります
+// 簡潔にするために空のままにしています
 static void on_device_event(const ChipDeviceEvent *event, intptr_t arg) {}
 static esp_err_t on_identification(identification::callback_type_t type, uint16_t endpoint_id,
-                                   uint8_t effect_id, uint8_t effect_variant, void *priv_data) {
+                   uint8_t effect_id, uint8_t effect_variant, void *priv_data) {
   return ESP_OK;
 }
 
-// Listener on attribute update requests.
-// In this example, when update is requested, path (endpoint, cluster and attribute) is checked
-// if it matches light attribute. If yes, LED changes state to new one.
+// 属性更新リクエストのリスナー。
+// この例では、更新がリクエストされたときに、パス（エンドポイント、クラスター、属性）がライト属性と一致するかどうかを確認します。
+// 一致する場合、LEDは新しい状態に変更されます。
 static esp_err_t on_attribute_update(attribute::callback_type_t type, uint16_t endpoint_id, uint32_t cluster_id,
-                                     uint32_t attribute_id, esp_matter_attr_val_t *val, void *priv_data) {
+                   uint32_t attribute_id, esp_matter_attr_val_t *val, void *priv_data) {
   if (type == attribute::PRE_UPDATE && endpoint_id == light_endpoint_id &&
-      cluster_id == CLUSTER_ID && attribute_id == ATTRIBUTE_ID) {
-    // We got an light on/off attribute update!
-    bool new_state = val->val.b;
-    digitalWrite(LED_PIN, new_state);
+    cluster_id == CLUSTER_ID && attribute_id == ATTRIBUTE_ID) {
+  // ライトのオン/オフ属性の更新を受け取りました！
+  bool new_state = val->val.b;
+  digitalWrite(LED_PIN, new_state);
   }
   return ESP_OK;
 }
@@ -61,57 +60,56 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(TOGGLE_BUTTON_PIN, INPUT);
 
-  // Enable debug logging
+  // デバッグログを有効にする
   esp_log_level_set("*", ESP_LOG_DEBUG);
 
-  // Setup Matter node
+  // Matterノードをセットアップする
   node::config_t node_config;
   node_t *node = node::create(&node_config, on_attribute_update, on_identification);
 
-  // Setup Light endpoint / cluster / attributes with default values
+  // デフォルト値でライトエンドポイント/クラスター/属性をセットアップする
   on_off_light::config_t light_config;
   light_config.on_off.on_off = false;
   light_config.on_off.lighting.start_up_on_off = false;
   endpoint_t *endpoint = on_off_light::create(node, &light_config, ENDPOINT_FLAG_NONE, NULL);
 
-  // Save on/off attribute reference. It will be used to read attribute value later.
+  // オン/オフ属性の参照を保存します。後で属性値を読み取るために使用されます。
   attribute_ref = attribute::get(cluster::get(endpoint, CLUSTER_ID), ATTRIBUTE_ID);
 
-  // Save generated endpoint id
+  // 生成されたエンドポイントIDを保存する
   light_endpoint_id = endpoint::get_id(endpoint);
   
-  // Setup DAC (this is good place to also set custom commission data, passcodes etc.)
+  // DACをセットアップする（ここはカスタムのコミッションデータ、パスコードなどを設定するのに適しています）
   esp_matter::set_custom_dac_provider(chip::Credentials::Examples::GetExampleDACProvider());
 
-  // Start Matter device
+  // Matterデバイスを起動する
   esp_matter::start(on_device_event);
 
-  // Print codes needed to setup Matter device
+  // Matterデバイスをセットアップするために必要なコードを印刷する
   PrintOnboardingCodes(chip::RendezvousInformationFlags(chip::RendezvousInformationFlag::kBLE));
 }
 
-// Reads light on/off attribute value
+// ライトのオン/オフ属性値を読み取る
 esp_matter_attr_val_t get_onoff_attribute_value() {
   esp_matter_attr_val_t onoff_value = esp_matter_invalid(NULL);
   attribute::get_val(attribute_ref, &onoff_value);
   return onoff_value;
 }
 
-// Sets light on/off attribute value
+// ライトのオン/オフ属性値を設定する
 void set_onoff_attribute_value(esp_matter_attr_val_t* onoff_value) {
   attribute::update(light_endpoint_id, CLUSTER_ID, ATTRIBUTE_ID, onoff_value);
 }
 
-// When toggle light button is pressed (with debouncing),
-// light attribute value is changed
+// トグルライトボタンが押されたとき（デバウンス処理付き）に、ライト属性値が変更される
 void loop() {
   if ((millis() - last_toggle) > DEBOUNCE_DELAY) {
-    if (!digitalRead(TOGGLE_BUTTON_PIN)) {
-      last_toggle = millis();
-      // Read actual on/off value, invert it and set
-      esp_matter_attr_val_t onoff_value = get_onoff_attribute_value();
-      onoff_value.val.b = !onoff_value.val.b;
-      set_onoff_attribute_value(&onoff_value);
-    }
+  if (!digitalRead(TOGGLE_BUTTON_PIN)) {
+    last_toggle = millis();
+    // 実際のオン/オフ値を読み取り、反転して設定する
+    esp_matter_attr_val_t onoff_value = get_onoff_attribute_value();
+    onoff_value.val.b = !onoff_value.val.b;
+    set_onoff_attribute_value(&onoff_value);
+  }
   }
 }
