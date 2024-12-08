9fd0f5d980040fb4c50f94dad4bf04d134f27392
wakky-alcedo <wakky.alcedo@gmail.com>
2024-12-09 02:11:41 +0900

    refactor(auto-curtain): using namespace を使わないようにした

--- auto-curtain/src/main.cpp
+++ auto-curtain/src/main.cpp
@@ -14,10 +14,8 @@
 #include "Matter.h"
 #include <app/server/OnboardingCodesUtil.h>
 #include <credentials/examples/DeviceAttestationCredsExample.h>
-using namespace chip;
-using namespace chip::app::Clusters;
-using namespace esp_matter;
-using namespace esp_matter::endpoint;
+namespace clusters = chip::app::Clusters;
+namespace em = esp_matter;
 
 // PINを設定してください
 const int LED_PIN = D0;
@@ -28,12 +26,12 @@ const int DEBOUNCE_DELAY = 500;
 int last_toggle;
 
 // Matterライトデバイスで使用されるクラスターと属性ID
-const uint32_t CLUSTER_ID = OnOff::Id;
-const uint32_t ATTRIBUTE_ID = OnOff::Attributes::OnOff::Id;
+const uint32_t CLUSTER_ID = clusters::OnOff::Id;
+const uint32_t ATTRIBUTE_ID = clusters::OnOff::Attributes::OnOff::Id;
 
 // Matterデバイスに割り当てられるエンドポイントと属性参照
 uint16_t light_endpoint_id = 0;
-attribute_t *attribute_ref;
+em::attribute_t *attribute_ref;
 
 
 // セットアッププロセスに関連するさまざまなデバイスイベントをリッスンする可能性があります
@@ -56,7 +54,7 @@ static void on_device_event(const ChipDeviceEvent *event, intptr_t arg) {}
   * @param priv_data プライベートデータ
   * @return esp_err_t 処理結果
   */
-static esp_err_t on_identification(identification::callback_type_t type, uint16_t endpoint_id,
+static esp_err_t on_identification(em::identification::callback_type_t type, uint16_t endpoint_id,
                    uint8_t effect_id, uint8_t effect_variant, void *priv_data) {
     return ESP_OK;
 }
@@ -76,9 +74,9 @@ static esp_err_t on_identification(identification::callback_type_t type, uint16_
   * @param priv_data プライベートデータ
   * @return esp_err_t 処理結果
   */
-static esp_err_t on_attribute_update(attribute::callback_type_t type, uint16_t endpoint_id, uint32_t cluster_id,
+static esp_err_t on_attribute_update(em::attribute::callback_type_t type, uint16_t endpoint_id, uint32_t cluster_id,
                    uint32_t attribute_id, esp_matter_attr_val_t *val, void *priv_data) {
-    if (type == attribute::PRE_UPDATE && endpoint_id == light_endpoint_id &&
+    if (type == em::attribute::PRE_UPDATE && endpoint_id == light_endpoint_id &&
         cluster_id == CLUSTER_ID && attribute_id == ATTRIBUTE_ID) {
         // ライトのオン/オフ属性の更新を受け取りました！
         bool new_state = val->val.b;
@@ -110,26 +108,26 @@ void setup() {
     esp_log_level_set("*", ESP_LOG_DEBUG);
 
     // Matterノードをセットアップする
-    node::config_t node_config;
-    node_t *node = node::create(&node_config, on_attribute_update, on_identification);
+    em::node::config_t node_config;
+    em::node_t *node = em::node::create(&node_config, on_attribute_update, on_identification);
 
     // デフォルト値でライトエンドポイント/クラスター/属性をセットアップする
-    on_off_light::config_t light_config;
+    em::endpoint::on_off_light::config_t light_config;
     light_config.on_off.on_off = false;
     light_config.on_off.lighting.start_up_on_off = false;
-    endpoint_t *endpoint = on_off_light::create(node, &light_config, ENDPOINT_FLAG_NONE, NULL);
+    em::endpoint_t *endpoint = em::endpoint::on_off_light::create(node, &light_config, em::ENDPOINT_FLAG_NONE, NULL);
 
     // オン/オフ属性の参照を保存します。後で属性値を読み取るために使用されます。
-    attribute_ref = attribute::get(cluster::get(endpoint, CLUSTER_ID), ATTRIBUTE_ID);
+    attribute_ref = em::attribute::get(em::cluster::get(endpoint, CLUSTER_ID), ATTRIBUTE_ID);
 
     // 生成されたエンドポイントIDを保存する
-    light_endpoint_id = endpoint::get_id(endpoint);
+    light_endpoint_id = em::endpoint::get_id(endpoint);
     
     // DACをセットアップする（ここはカスタムのコミッションデータ、パスコードなどを設定するのに適しています）
-    esp_matter::set_custom_dac_provider(chip::Credentials::Examples::GetExampleDACProvider());
+    em::set_custom_dac_provider(chip::Credentials::Examples::GetExampleDACProvider());
 
     // Matterデバイスを起動する
-    esp_matter::start(on_device_event);
+    em::start(on_device_event);
 
     // Matterデバイスをセットアップするために必要なコードを印刷する
     PrintOnboardingCodes(chip::RendezvousInformationFlags(chip::RendezvousInformationFlag::kBLE));
@@ -142,7 +140,7 @@ void setup() {
   */
 esp_matter_attr_val_t get_onoff_attribute_value() {
     esp_matter_attr_val_t onoff_value = esp_matter_invalid(NULL);
-    attribute::get_val(attribute_ref, &onoff_value);
+    em::attribute::get_val(attribute_ref, &onoff_value);
     return onoff_value;
 }
 
@@ -152,7 +150,7 @@ esp_matter_attr_val_t get_onoff_attribute_value() {
   * @param onoff_value ライトのオン/オフ属性値
   */
 void set_onoff_attribute_value(esp_matter_attr_val_t* onoff_value) {
-    attribute::update(light_endpoint_id, CLUSTER_ID, ATTRIBUTE_ID, onoff_value);
+    em::attribute::update(light_endpoint_id, CLUSTER_ID, ATTRIBUTE_ID, onoff_value);
 }
 
 /**
